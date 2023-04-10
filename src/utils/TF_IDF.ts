dc7bd291963ba90ca32880b7672769f20fe400b0
Sergio Herrera <sergio.herrera02@utrgv.edu>
2023-04-09 20:29:10 -0500

    tfidf complete

---
+++ src/utils/TF_IDF.ts
@@ -0,0 +1,58 @@
+import BioTokenizer from '../utils/BioTokenizer';
+
+class TFIDF {
+
+    private bioTokens: {[id: string]: string[]};
+  
+    constructor(bioTokens: {[id: string]: string[]}) {
+      this.bioTokens = bioTokens;
+    }
+  
+    private computeTF(bio: string[], terms: string[]): {[term: string]: number} {
+      const termFrequency: {[term: string]: number} = {};
+      const totalTerms = bio.length;
+      terms.forEach((term) => {
+        const count = bio.filter((word) => word === term).length;
+        termFrequency[term] = count / totalTerms;
+      });
+      return termFrequency;
+    }
+  
+    private computeIDF(bios: string[][], term: string): number {
+      let count = 0;
+      for (let i = 0; i < bios.length; i++) {
+        if (bios[i].includes(term)) {
+          count++;
+        }
+      }
+      return Math.log(bios.length / (count + 1));
+    }
+  
+    public computeTFIDF(): {[id: string]: {[term: string]: number}} {
+      const tfidf: {[id: string]: {[term: string]: number}} = {};
+      const bios: string[][] = Object.values(this.bioTokens);
+      const allTerms: string[] = Array.from(new Set(bios.flat()));
+  
+      for (const [username, bio] of Object.entries(this.bioTokens)) {
+        const tf: {[term: string]: number} = this.computeTF(bio, allTerms);
+        tfidf[username] = {};
+        for (const term of Object.keys(tf)) {
+          const idf = this.computeIDF(bios, term);
+          if (tf[term] * idf > 0){
+            tfidf[username][term] = tf[term] * idf;
+          }
+          
+        }
+      }
+  
+      return tfidf;
+    }
+  }
+  
+  const tokenizer = new BioTokenizer();
+  const bioTokens = tokenizer.getBioTokens();
+  
+  const tfidf = new TFIDF(bioTokens);
+  export const tfidfScores = tfidf.computeTFIDF();
+
+  
